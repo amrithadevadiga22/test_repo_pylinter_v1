d22047d7cf87bc3e0a6b328306ad66a8a126e02e
Amritha <amritha.devadiga@fractal.ai>
2025-05-05 17:21:06 +0530

    added all project files

--- data.py
+++ data.py
@@ -4,3 +4,21 @@ import kagglehub
 path = kagglehub.dataset_download("prachi13/customer-analytics")
 
 print("Path to dataset files:", path)
+
+
+curl -X POST "http://localhost:8000/predict" \
+     -H "Content-Type: application/json" \
+     -d '{
+           "Warehouse_block": "F",
+           "Mode_of_Shipment": "Ship",
+           "Customer_care_calls": 4,
+           "Customer_rating": 2,
+           "Cost_of_the_Product": 177,
+           "Prior_purchases": 3,
+           "Product_importance": "low",
+           "Gender": "F",
+           "Discount_offered": 44,
+           "Weight_in_gms": 1233
+         }'
+
+docker run -d -p 8000:8000 your-dockerhub-username/ml-fastapi-app:latest
